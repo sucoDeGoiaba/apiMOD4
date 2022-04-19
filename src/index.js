ecd9548ba50a740130e6419879f2b90e0b54a1e3
sucoDeGoiaba <flp.pxxs@gmail.com>
2022-04-19 09:37:17 -0300

    renomeando e adicionando cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import controllerEstoque from './controller/controllerEstoque.js'
 import db from './database/sqliteEstoque.js'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 controllerEstoque(app, db)
